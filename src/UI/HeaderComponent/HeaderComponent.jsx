5a87f5ba98ab4e5eef2670acce00c747fa3a8635
chidera-collins <chideracollins24@gmail.comm>
2025-04-29 00:13:53 +0100

    header

--- src/UI/HeaderComponent/HeaderComponent.jsx
+++ src/UI/HeaderComponent/HeaderComponent.jsx
@@ -155,18 +155,12 @@ function HeaderComponent() {
                         <li className='hover:text-text'><Link to={'/'} onClick={navWidth}>Home</Link></li>
                         <li className='hover:text-text'><Link to={'/about'} onClick={navWidth}>About</Link></li>
                         <li className='hover:text-text'><Link to={'/shop'} onClick={navWidth}>Shop</Link></li>
-                        <li className='group'>Pages
-                            <div className='  group-hover:block bg-[#808080a3] w-[full] mt-5  h-[140px]'>
-                                <ul className='grid grid-cols-1 h-[inherit]'>
                                     <li className=' w-full text-center hover:text-text' onClick={navWidth}><Link to={'/cart'}>Cart</Link></li>
                                     <li className=' w-full text-center hover:text-text' onClick={navWidth}><Link to={'/checkout'}>Checkout</Link></li>
                                     <li className=' w-full text-center hover:text-text' onClick={navWidth}><Link to={'/services'}>Services</Link></li>
                                     <li className=' w-full text-center hover:text-text'onClick={navWidth}><Link to={'/faq'}>Faq</Link></li>
-                                </ul>
 
-                            </div>
 
-                        </li>
                         <li className='hover:text-text' onClick={navWidth}><Link to={'/contact'}>Contact</Link></li>
 
 
